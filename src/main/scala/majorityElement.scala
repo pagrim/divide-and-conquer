bf07952002a26216615344bde918d707c7179f52
Peter Grimshaw <pagrimshaw@gmail.com>
2022-06-08 16:47:09 +0100

    Simple implementation of majority element with counting

--- src/main/scala/majorityElement.scala
+++ src/main/scala/majorityElement.scala
@@ -1,60 +1,31 @@
 import scala.io.StdIn.{readInt, readLine}
+import scala.annotation.tailrec
 
 object majorityElement {
 
-  def getMajority(input: Array[Long]): Long = {
-    val majority = _getMajority(input, 0, input.length -1)
-    val majorityCount = countFrequency(input, 0, input.length -1, majority)
-    if(majorityCount > input.length/2){
-    majority} else {
-      0
-    }
+  def getMajority(input: Array[Long]): (Long, Int) = {
+    val majorityThresh = input.length/2
+    val counts = input.groupBy(identity).map(grp => (grp._1, grp._2.length)).toArray
+    fetchMajority(counts, 0, majorityThresh)
   }
 
-  def isMajority(input: Array[Long]): Long = {
-    val majority = _getMajority(input, 0, input.length -1)
-    val majorityCount = countFrequency(input, 0, input.length -1, majority)
-    if(majorityCount > input.length/2){
-      1} else {
-      0
-    }
-  }
-
-
-
-  def _getMajority(input: Array[Long], leftIndex: Int, rightIndex: Int): Long = {
-
-    if( leftIndex == rightIndex) {
-      input(leftIndex)
+  @tailrec
+  def fetchMajority(counts: Array[(Long, Int)], idx: Int, majorityThresh: Int): (Long, Int) = {
+    if (idx == counts.length) { (-1L, 0) }
+    else if (counts(idx)._2 >= majorityThresh) {
+      (counts(idx)._1, 1)
     } else {
-
-      val midPoint = (leftIndex + rightIndex)/2
-
-      val leftMajority = _getMajority(input, 0, midPoint)
-      val rightMajority = _getMajority(input, midPoint + 1, rightIndex)
-
-      val leftFreq = countFrequency(input, leftIndex, rightIndex, leftMajority)
-      val rightFreq = countFrequency(input, leftIndex, rightIndex, rightMajority)
-
-      if(leftFreq > rightFreq){
-        leftMajority
-      } else {
-        rightMajority
-      }
-
+      fetchMajority(counts, idx + 1, majorityThresh)
     }
+}
 
-  }
-
-  def countFrequency(input: Array[Long], leftIndex: Int, rightIndex: Int, target: Long): Int = {
-    input.slice(leftIndex, rightIndex +1).count( x => x == target)
-  }
 
   def main(args: Array[String]): Unit = {
     val numElems = readInt()
     val input = readLine().split(" ").map(el => el.toLong)
     assert(input.length == numElems)
-    print(isMajority(input))
+    val (_, flag) = getMajority(input)
+    print(flag)
   }
 
 
