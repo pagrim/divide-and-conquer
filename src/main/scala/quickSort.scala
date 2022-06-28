f17c03075978ca5e0b4535978a93fabbd09d2c93
Peter Grimshaw <pagrimshaw@gmail.com>
2022-06-28 21:15:28 +0100

    Rewrote quicksort in imperative style, no returning of array

--- src/main/scala/quickSort.scala
+++ src/main/scala/quickSort.scala
@@ -1,30 +1,17 @@
 import scala.io.StdIn.{readInt, readLine}
-
-import scala.annotation.tailrec
 import scala.util.Random
 import com.typesafe.scalalogging.Logger
-
-case class Indices(left: Int, right: Int, pivot: Int, lower: Int, equal: Int, scan: Int)
-
-case class PartitionState(arr: Array[Int], lowerIdx: Int, equalIdx: Int, scanIdx: Int) {
-
-  override def equals(other: Any): Boolean = other match {
-    case ps: PartitionState =>
-      ps.arr.sameElements(this.arr) && ps.lowerIdx == this.lowerIdx && ps.equalIdx == this.equalIdx &&
-        ps.scanIdx == this.scanIdx
-  }
-
-  override def toString: String = {
-    val arrRep = this.arr.mkString(sep = ",")
-    s"arr:(${arrRep}),lowerIdx:${this.lowerIdx},equalIdx:${this.equalIdx},scanIdx:${this.scanIdx}"
-  }
-}
 
 object quickSort {
 
   val logger: Logger = Logger("quicksort")
 
-  def sort(input: Array[Int], randomSeed: Option[Int] = None): Array[Int] = {
+  def sort(arr: Array[Int], randomSeed: Option[Int] = None): Unit = {
+    /** *
+     * This function runs the quicksort algorithm. It picks a random pivot and puts this at the rightmost position of
+     * the array, then runs the partition algo to get the lower, mid and upper elements. The upper and lower parts
+     * are then sorted to get the complete sort
+     */
     val rdm = randomSeed match {
       case Some(i) => new Random(i)
       case _ =>
@@ -33,104 +20,53 @@ object quickSort {
         logger.info(s"Using random seed ${seed}")
         new Random(seed)
       }
-    _sort(input, 0, input.length, rdm, "root")
+    _sort(arr, 0, arr.length - 1, rdm)
   }
 
-  def _sort(input: Array[Int], leftIndex: Int, rightIndex: Int, randomGen: Random, desc: String): Array[Int] = {
-    /** *
-     * This function runs the quicksort algorithm. It picks a random pivot and puts this at the leftIndex position at
-     * the start of the array, then runs the partition algo to get the position of the pivot. The upper and lower parts
-     * are then sorted to get the complete sort
-     * Uses the convention that if:
-     * right == left => 0 elements
-     * right = left + 1 => 1 element
-     * right = left + 2 => 2 elements
-     * right > left + 2 => 3 or more elements and run sorting
-     * i.e. for the purpose of this function the rightIndex is exclusive
-     */
-    rightIndex match {
-      case ri if(ri == leftIndex) => Array()
-      case ri if(ri == leftIndex + 1) => Array(input(leftIndex))
-      case ri if(ri == leftIndex + 2) =>
-        if (input(rightIndex -1) >= input(leftIndex)) input.slice(leftIndex, rightIndex)
-        else {
-          Array(input(rightIndex-1), input(leftIndex))
-        }
-      case _ => {
-        logger.debug(s"Starting main _sort on input: ${input}, range: ${desc}, leftIndex: ${leftIndex}, rightIndex: ${rightIndex}")
-        val randIndex = randomGen.nextInt(rightIndex - leftIndex) + leftIndex
-        val firstElement = input(leftIndex)
-        val randPivotChosen = input.updated(leftIndex, input(randIndex)).updated(randIndex, firstElement)
-        val partitionRes = partition(
-          arr = randPivotChosen,
-          Indices(
-            left = leftIndex,
-            right = rightIndex -1,
-            pivot = leftIndex,
-            lower = leftIndex,
-            equal = leftIndex,
-            scan = leftIndex + 1)
-        )
-        val lowerSorted = _sort(partitionRes.arr, leftIndex, partitionRes.lowerIdx + 1, randomGen, "lower")
-        logger.debug(s"lowerSorted ${lowerSorted}")
-        val equalSorted = partitionRes.arr.slice(math.min(partitionRes.lowerIdx + 1, partitionRes.equalIdx), partitionRes.equalIdx + 1)
-        logger.debug(s"equalSorted: ${equalSorted}")
-        val upperSorted = _sort(partitionRes.arr, math.min(partitionRes.equalIdx + 1, rightIndex -1), rightIndex, randomGen, "upper")
-        logger.debug(s"upperSorted: ${upperSorted}")
-        val concatSorted = lowerSorted ++ equalSorted ++ upperSorted
-        logger.debug(s"concat: ${concatSorted}")
-        concatSorted
-      }
+  def _sort(arr: Array[Int], left: Int, right: Int, rdm: Random): Unit = {
+    if (left < right) {
+      logger.debug(s"Starting main sort")
+      val randIndex = rdm.nextInt(arr.length)
+      swap(arr, randIndex, arr.length -1)
+      val (j, k) = partition(arr, left, right)
+      _sort(arr, left, j-1, rdm)
+      _sort(arr, k+1, right, rdm)
     }
   }
 
-  @tailrec
-  def partition(arr: Array[Int], indices: Indices): PartitionState = {
-    /** *
-     * This function implements updates on the partitioned part of the array until the left index reaches the right
-     * index, then swaps the pivot to its final position and reduce the lower index in the case of a swap
-     */
-    val pivotVal = arr(indices.pivot)
-    val updated = partitionUpdate(arr, pivotVal, indices.scan, indices.lower, indices.equal)
-    if (indices.scan >= indices.right) {
-      val swappedPivot = updated.arr.updated(indices.pivot, updated.arr(updated.lowerIdx)).updated(updated.lowerIdx, pivotVal)
-      val lowerIdxAdjustment = if (indices.lower > indices.pivot) 1 else 0
-      return PartitionState(swappedPivot, updated.lowerIdx - lowerIdxAdjustment, updated.equalIdx, updated.scanIdx)
-    } else {
+
+  def partition(arr: Array[Int], left: Int, right: Int): (Int, Int) = {
+    /** This function implements a partition on an array */
+    val pivotVal = arr(arr.length)
+    var (j, k) = (0, 0)
+    for (i <- left until right) {
+        arr(i) match {
+         case x if x == pivotVal =>
+           j += 1
+           swap(arr, i, j)
+         case x if x < pivotVal =>
+           j += 1
+           k += 1
+           swap(arr, i, j)
+           swap(arr, i, k)
+       }
     }
-    val newIndices = Indices(indices.left, indices.right, indices.pivot, updated.lowerIdx, updated.equalIdx, indices.scan + 1)
-    partition(arr=updated.arr, newIndices)
+    (j, k)
   }
 
-  def partitionUpdate(arr: Array[Int], pivotVal: Int, scanIdx: Int, lowerIdx: Int, equalIdx: Int): PartitionState = {
-    /** *
-     * Given a scanElement being considered this function returns the updated array after the positioning the scanElement
-     * in the lower, equal or upper part of the array (the latter requiring no swaps since the scanElement is in this
-     * by default)
-     */
-    val scanElement = arr(scanIdx)
-    scanElement match {
-      case x if x == pivotVal =>
-        val newEqualIdx = equalIdx + 1
-        val firstSwap = arr.updated(scanIdx, arr(newEqualIdx)).updated(newEqualIdx, scanElement)
-        PartitionState(firstSwap, lowerIdx, newEqualIdx, scanIdx)
-      case x if x < pivotVal =>
-        val newLowerIdx = lowerIdx + 1
-        val newEqualIdx = equalIdx + 1
-        val firstSwap = arr.updated(scanIdx, arr(newLowerIdx)).updated(newLowerIdx, scanElement)
-        val tempScanEl = firstSwap(scanIdx)
-        val secondSwap = firstSwap.updated(scanIdx, firstSwap(newEqualIdx)).updated(newEqualIdx, tempScanEl)
-        PartitionState(secondSwap, newLowerIdx, newEqualIdx, scanIdx)
-      case _ =>
-        PartitionState(arr, lowerIdx, equalIdx, scanIdx)
-    }
+  def swap(arr: Array[Int], idx1: Int, idx2: Int): Unit = {
+    val temp = arr(idx1)
+    arr(idx1) = arr(idx2)
+    arr(idx2) = temp
   }
+
 
   def main(args: Array[String]): Unit = {
     val numElems = readInt()
     val input = readLine().split(" ").map(el => el.toInt)
     assert(input.length == numElems)
-    print(sort(input).mkString(" "))
+    sort(input)
+    print(input.mkString(" "))
   }
 
 }
--- src/test/scala/QuickSortSpec.scala
+++ src/test/scala/QuickSortSpec.scala
@@ -6,58 +6,34 @@ import org.scalatest.prop.TableDrivenPropertyChecks.{Table, forAll}
 class QuickSortSpec extends AnyFlatSpec with Matchers {
 
   "A quickSort" should "sort input array correctly" in {
-    quickSort.sort(Array(4, 1, 5, 8, 9, 5), randomSeed = Some(42)) shouldEqual Array(1, 4, 5, 5, 8, 9)
-    quickSort.sort(Array(6, 7, 3, 3, 0), randomSeed = Some(42)) shouldEqual Array(0, 3, 3, 6, 7)
-    quickSort.sort(Array(2, 3, 9, 2, 9), randomSeed = Some(-941176648)) shouldEqual Array(2, 2, 3, 9, 9)
+
+    val sortExamples = Table(("array", "randomSeed", "expRes"),
+      (Array(4, 1, 5, 8, 9, 5), Some(42), Array(1, 4, 5, 5, 8, 9)),
+      (Array(6, 7, 3, 3, 0), Some(42), Array(0, 3, 3, 6, 7)),
+      (Array(2, 3, 9, 2, 9), Some(-941176648), Array(2, 2, 3, 9, 9))
+    )
+
+    forAll(sortExamples) {
+      (array: Array[Int], randomSeed: Option[Int], expRes: Array[Int]) =>
+        quickSort.sort(array, randomSeed = randomSeed)
+        array shouldEqual expRes
+    }
   }
 
   "A quickSort" should "partition input array correctly" in {
 
     val partitionExamples = Table(
-      ("arr", "lowerIdx", "equalIdx", "scanIdx", "resArr", "resLowerIdx",
-        "resEqualIdx", "resScanIdx"),
-      (Array(4, 1, 5, 8, 9, 5), 0, 0, 1, Array(1, 4, 5, 8, 9, 5), 0, 1, 5),
-      (Array(6, 1, 5, 8, 6, 6, 9, 5), 0, 0, 1, Array(5, 1, 5, 6, 6, 6, 9, 8), 2, 5, 7),
-      (Array(6, 1, 5, 8, 6, 6, 9, 5), 0, 0, 1, Array(5, 1, 5, 6, 6, 6, 9, 8), 2, 5, 7)
+      ("arr", "resArr", "j", "k"),
+      (Array(4, 1, 5, 8, 9, 5), Array(1, 4, 5, 8, 9, 5), 0, 1),
+      (Array(6, 1, 5, 8, 6, 6, 9, 5), Array(5, 1, 5, 6, 6, 6, 9, 8), 2, 5),
+      (Array(6, 1, 5, 8, 6, 6, 9, 5), Array(5, 1, 5, 6, 6, 6, 9, 8), 2, 5)
     )
 
     forAll(partitionExamples) {
-      (arr: Array[Int], lowerIdx: Int, equalIdx: Int, scanIdx: Int, resArr: Array[Int],
-       resLowerIdx: Int, resEqualIdx: Int, resScanIdx: Int) =>
-        val expPartitionState = PartitionState(resArr, resLowerIdx, resEqualIdx, resScanIdx)
-        val res = quickSort.partition(
-          arr = arr,
-          Indices(
-            left=0,
-            right=arr.length - 1,
-            pivot=lowerIdx,
-            lower=lowerIdx,
-            equal=equalIdx,
-            scan=scanIdx)
-          )
-        res shouldEqual expPartitionState
+      (arr: Array[Int], resArr: Array[Int], j: Int, k: Int) =>
+        val res = quickSort.partition(arr = arr, left = 0, right = arr.length - 1)
+        arr shouldEqual resArr
+        res shouldEqual(j, k)
     }
   }
-
-  "A PartitionState" should "evaluate equals correctly" in {
-    PartitionState(Array(0, 1, 2), 0, 1, 2) shouldEqual PartitionState(Array(0, 1, 2), 0, 1, 2)
-  }
-
-  "A quickSort" should "update input array correctly for partitioning" in {
-    val partitionUpdateExamples = Table(
-      ("arr", "pivotVal", "scanIdx", "lowerIdx", "equalIdx", "resArr", "resLowerIdx", "resEqualIdx"),
-      (Array(6, 4, 1, 5, 8, 9, 5), 6, 4, 3, 3, Array(6, 4, 1, 5, 8, 9, 5), 3, 3),
-      (Array(6, 4, 1, 6, 6, 8, 9, 5, 3), 6, 7, 2, 4, Array(6, 4, 1, 5, 6, 6, 9, 8, 3), 3, 5)
-    )
-
-    forAll(partitionUpdateExamples) {
-      (arr: Array[Int], pivotVal: Int, scanIdx: Int, lowerIdx: Int,
-       equalIdx: Int, resArr: Array[Int], resLowerIdx: Int, resEqualIdx: Int) =>
-        val partitionUpdateRes = quickSort.partitionUpdate(arr, pivotVal, scanIdx, lowerIdx, equalIdx)
-        partitionUpdateRes.arr shouldEqual resArr
-        partitionUpdateRes.lowerIdx shouldEqual resLowerIdx
-        partitionUpdateRes.equalIdx shouldEqual resEqualIdx
-    }
-  }
-
 }
